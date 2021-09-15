f4a8dadd94e6038c4307e72ff6b7ae428544e043
susydesign <tarnavz@gmail.com>
2021-09-15 14:24:34 +0200

    Alap nezet

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <!-- >
+            1Pt2,15
+            "Mert Isten akarata az, hogy jót cselekedve némítsátok el az értelmetlen emberek tudatlanságát"
+    -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vistor Care</title>
+    <meta name="googlebot" content="noindex">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="./view/css/style.css">
+
+</head>
+<body>
+    <div id="header_vc" class="container-fluid">
+            <div id="header_1sor" class="row">
+                <div class="col-1 d-none d-md-block"></div>
+                <div class="col-10">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="../view/images/svg/Visitor-Care-logo.svg" alt="visitor care logo fekete">
+                        </div>
+                        <div class="col-md-3" style="padding-top: 5px;">
+                            <p class="text-end">Ügyfélszolgálat:<span>+36 (30) 123-4567</span></p>
+                        </div>
+                        <div class="col-md-3" style="padding-top: 5px;">
+                            <p class="text-start">E-mail:<span>info@visitorcare.eu</span></p>
+                        </div>
+                        <div class="col-md-1 col-sm-6">
+                            <button class="btn">Belépés</button>
+                        </div>
+                        <div class="col-md-1 col-sm-6">
+                            <button class="btn">Regisztráció</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-1 d-none d-md-block"></div>
+            </div>
+
+            <div id="navigacio" class="row">
+            <div class="col-1 d-none d-md-block"></div>
+                <div class="col-10">
+                    <div class="row">
+                        <div class="col-md-8 text-start">
+                            <p>Regisztrált szakemberek az oldalon: <span>1.234 db</span></p>
+                        </div>
+                        <div class="col-md-4 justify-content-end">
+                            <nav class="nav">
+                                <a class="nav-link active" href="">Főoldal</a>
+                                <a class="nav-link" href="#">Rólunk</a>
+                                <a class="nav-link" href="#">Friss hírek</a>
+                                <a class="nav-link" href="#">Kapcsolat</a>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-1 d-none d-md-block"></div>
+            </div>
+    </div>
+
+    <div id="hirek" class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Legfrissebb Hírek</h1>
+            </div>
+        </div>
+    </div>
+
+    <div id="valaszdki" class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Válaszd ki, kit keresel!</h1>
+            </div>
+        </div>
+    </div>
+
+    <div id="hogyan" class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Hogyan működik a Visitor Care?</h1>
+            </div>
+        </div>
+    </div>
+
+    <div id="bestszakik" class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Legnépszerűbb szakemberek</h1>
+            </div>
+        </div>
+    </div>
+
+    <div id="pinks" class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Pink kockák</h1>
+            </div>
+        </div>
+    </div>
+
+    <div id="prartnerink" class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Partnereink</h1>
+            </div>
+        </div>
+    </div>
+
+    <div id="footer_vc" class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Lábléc</h1>
+            </div>
+            <div class="col-md-12">
+                <p>Minden jog fenntartva</p>
+            </div>
+        </div>    
+    </div>
+
+    
+
+
+
+    <!-- Scriptek -->  
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" 
+    crossorigin="anonymous"></script>
+    <script src="./js/hw.js"></script>
+</body>
+</html>
